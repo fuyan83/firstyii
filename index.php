030a4ab43152d4571604b625c8458acadb2abf6c
fuxiongjun <343662210@qq.com>
2015-07-25 17:26:51 +0800

    add change1

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 define('APP_DEBUG',false); // 是否调试模式
 define('APP_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);//系统根目录
 require APP_PATH.'Core/Runtime.php';//加载系统启动文件
-echo "fasfads12121200000432432432bb";
+echo "fasfads12121200000432432432cc";
