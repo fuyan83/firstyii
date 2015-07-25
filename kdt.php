63b3d7ee3d87ec309374cb0839bb1027996e3f7b
fuxiongjun <343662210@qq.com>
2015-07-25 23:00:09 +0800

    and simple

--- kdt.php
+++ kdt.php
@@ -12,7 +12,7 @@ require APP_PATH.'Core/Runtime.php';//加载系统启动文件
 set_time_limit(0);
 //echo PHP_EOL;
 DB::init();
-echo 'fdsafadsfdas12121244555dd1';
+echo 'fdsafadsfdas12121244555eeee1';
 
 require APP_PATH.'kdt/KdtApiClient.php';
 
